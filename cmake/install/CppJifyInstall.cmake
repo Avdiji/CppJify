1db48ac9752ca6beee114f062dec0e2fdf0426e7
MAIN\Fitor Avdiji <favdiji@googlemail.com>
2025-03-25 14:24:48 +0100

    adjusted install target and conanfile

--- cmake/install/CppJifyInstall.cmake
+++ cmake/install/CppJifyInstall.cmake
@@ -1,39 +1,45 @@
-# INSTALL
+# Set the install prefix to the desired root
+set(CMAKE_INSTALL_PREFIX ${CMAKE_BINARY_DIR}/install)
+
+# Install the library
 install(
     TARGETS ${CPP_JIFY}
     EXPORT ${CPP_JIFY}Targets
-
     FILE_SET HEADERS
-    DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
+    DESTINATION include
     ARCHIVE DESTINATION lib
     LIBRARY DESTINATION lib
     RUNTIME DESTINATION bin
     INCLUDES DESTINATION include
 )
 
+# Install the export file
 install(
     EXPORT ${CPP_JIFY}Targets
     FILE ${CPP_JIFY}Targets.cmake
     # NAMESPACE ${CPP_JIFY}:: no namespace for this package
-    DESTINATION ${CMAKE_INSTALL_PREFIX}/cmake
+    DESTINATION cmake
 )
 
+# Generate version file
 include(CMakePackageConfigHelpers)
 write_basic_package_version_file(
-    ${CMAKE_INSTALL_PREFIX}/cmake/${CPP_JIFY}ConfigVersion.cmake
+    ${CMAKE_BINARY_DIR}/cmake/${CPP_JIFY}ConfigVersion.cmake
     VERSION 1.0
     COMPATIBILITY SameMajorVersion
 )
 
+# Configure the package config file
 configure_package_config_file(
     ${CMAKE_CURRENT_LIST_DIR}/Config.cmake.in
-    ${CMAKE_INSTALL_PREFIX}/cmake/${CPP_JIFY}Config.cmake
-    INSTALL_DESTINATION lib/cmake/Project${CPP_JIFY}
+    ${CMAKE_BINARY_DIR}/cmake/${CPP_JIFY}Config.cmake
+    INSTALL_DESTINATION cmake
 )
 
+# Install the package config and version files
 install(
     FILES
-    ${CMAKE_INSTALL_PREFIX}/cmake/${CPP_JIFY}Config.cmake
-    ${CMAKE_INSTALL_PREFIX}/cmake/${CPP_JIFY}ConfigVersion.cmake
-    DESTINATION ${CMAKE_INSTALL_PREFIX}/cmake
+    ${CMAKE_BINARY_DIR}/cmake/${CPP_JIFY}Config.cmake
+    ${CMAKE_BINARY_DIR}/cmake/${CPP_JIFY}ConfigVersion.cmake
+    DESTINATION cmake
 )
